bb91769c30e60d32c62182d9335aa42ffe042af0
Shaktalaz <shaktalaz@live.de>
2018-09-15 23:03:11 +0200

    Add project

---
+++ KEVIN IST FAGIOTTO/Assets/MyScripts/Camera_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour {
+    public float speed = 10f;
+
+    CharacterController camera;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveDirection *= speed;
+        camera.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ KEVIN IST FAGIOTTO/Assets/MyScripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    
+    Renderer renderer = null;
+
+    GameObject selection = null;
+
+    Material originalMarerial;
+    GameObject originalSelection;
+
+    public Material highlightMaterial;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+
+    void SelectGameObject(GameObject currentSelection)
+    {
+        if(!currentSelection.Equals(selection))
+        {
+            if (selection != null)
+            {
+                //Altes Material wiederherstellen
+                selection.GetComponent<Renderer>().sharedMaterial = originalMarerial;
+
+            }
+
+            //Material zwischenspeichern(für deselection)
+            originalMarerial = currentSelection.GetComponent<Renderer>().sharedMaterial;
+
+            //GameObject selecten
+            selection = currentSelection;
+
+            //GameObject als selected markieren
+            selection.GetComponent<Renderer>().sharedMaterial = highlightMaterial;
+        }
+    }
+
+    void DeselectGameObject()
+    {
+        if(selection != null)
+        {
+            //Ursprüngliches Material wiederherstellen
+            selection.GetComponent<Renderer>().sharedMaterial = originalMarerial;
+
+            //GameObject deselecten
+            selection = null;
+        }
+
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if(selection != null)
+        {
+            Debug.Log(selection.transform.name);
+        }
+        
+        //Wenn linksclick
+		if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse left-klick.");
+
+            Ray mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit hit;
+            if(Physics.Raycast((mousePoint), out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "Selectable")
+                {
+
+                    Debug.DrawRay(mousePoint.origin, mousePoint.direction * hit.distance, Color.green);
+                    Debug.Log("Hit Object: " + hit.transform.gameObject.name);
+
+                    SelectGameObject(hit.transform.gameObject);
+                }
+                else
+                {
+                    DeselectGameObject();
+                    Debug.DrawRay(mousePoint.origin, mousePoint.direction * hit.distance, Color.red);
+                }
+            }
+            else
+            {
+                DeselectGameObject();
+                Debug.DrawRay(mousePoint.origin, mousePoint.direction * 1000, Color.white);
+            }
+        }
+	}
+}
